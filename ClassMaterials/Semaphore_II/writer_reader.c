07575410b3cfaa93915efcacf4b77d2249019003
Lixing Song <song3@rose-hulman.edu>
2019-01-17 22:08:57 -0500

    Semaphore lecture 2

---
+++ ClassMaterials/Semaphore_II/writer_reader.c
@@ -0,0 +1,60 @@
+/* Adapted from https://gist.github.com/rajabishek/6209a575f00b122fe490 */
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+
+
+sem_t mutex,writeblock;
+int data = 0,rcount = 0;
+
+void *reader(void *arg)
+{
+    int f;
+    f = *((int*)arg);
+    while(1){
+
+        //critical section
+        printf("Data read by the reader%d is %d\n",f,data);
+        sleep(1);
+        //critical section
+
+        usleep(200);
+    }
+}
+
+void *writer(void *arg)
+{
+    int f;
+    f = *((int*) arg);
+    while(1){
+
+        //critical section
+        printf("Writer%d starts to write..\n",f);
+        data++;
+        sleep(1);
+        printf("Writer%d finishes to write %d\n",f,data);
+        //critical section
+        
+        sleep(1);
+    }
+}
+
+int main()
+{
+    int i,b; 
+    pthread_t rtid[5],wtid[5];
+    sem_init(&mutex,0,1);
+    sem_init(&writeblock,0,1);
+    //create writer
+    for(i=0;i<=1;i++)
+        pthread_create(&wtid[i],NULL,writer,(void *)&i);
+    //create readers
+    for(i=0;i<=4;i++)
+        pthread_create(&rtid[i],NULL,reader,(void *)&i);
+
+    pthread_join(wtid[0],NULL);
+
+
+    return 0;
+}
---
+++ ClassMaterials/Semaphore_II/writer_reader_soln_v2.c
@@ -0,0 +1,70 @@
+/* Adapted from https://gist.github.com/rajabishek/6209a575f00b122fe490 */
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+
+
+sem_t mutex,writeblock, non_starv;
+int data = 0,rcount = 0;
+
+void *reader(void *arg)
+{
+    int f;
+    f = *((int*)arg);
+    while(1){
+        sem_wait(&non_starv);
+        sem_post(&non_starv);
+        sem_wait(&mutex);
+        rcount = rcount + 1;
+        if(rcount==1)
+            sem_wait(&writeblock);
+        sem_post(&mutex);
+        printf("Data read by the reader%d is %d\n",f,data);
+        sleep(1);
+        sem_wait(&mutex);
+        rcount = rcount - 1;
+        if(rcount==0)
+            sem_post(&writeblock);
+        sem_post(&mutex);
+
+        /* usleep(200); */
+    }
+}
+
+void *writer(void *arg)
+{
+    int f;
+    f = *((int*) arg);
+    while(1){
+        sem_wait(&non_starv);
+        sem_wait(&writeblock);
+        printf("Writer%d starts to write..\n",f);
+        data++;
+        sleep(1);
+        sem_post(&writeblock);
+        sem_post(&non_starv);
+        printf("Writer%d finishes to write %d\n",f,data);
+        sleep(1);
+    }
+}
+
+int main()
+{
+    int i,b; 
+    pthread_t rtid[5],wtid[5];
+    sem_init(&mutex,0,1);
+    sem_init(&writeblock,0,1);
+    sem_init(&non_starv,0,1);
+    //create writer
+    for(i=0;i<=1;i++)
+        pthread_create(&wtid[i],NULL,writer,(void *)&i);
+    //create readers
+    for(i=0;i<=4;i++)
+        pthread_create(&rtid[i],NULL,reader,(void *)&i);
+
+    pthread_join(wtid[0],NULL);
+
+
+    return 0;
+}
