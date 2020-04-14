da84e4b84db0a699c916aef8bb4e6816c909f914
Michael Hewner <hewner@gmail.com>
2020-04-14 11:17:27 -0400

    Semaphores II lecture

--- ClassMaterials/Semaphore_II/writer_reader.c
+++ ClassMaterials/Semaphore_II/writer_reader.c
@@ -6,6 +6,8 @@
 
 
 int data = 0;
+sem_t writeblock, mutex;
+int readers_in_cs = 0;
 
 void *reader(void *arg)
 {
@@ -13,11 +15,27 @@ void *reader(void *arg)
     f = *((int*)arg);
     while(1){
 
+        sem_wait(&mutex)
+        if(readers_in_cs == 0) {
+            sem_wait(&writeblock);
+        }
+        readers_in_cs++;
+        sem_post(&mutex);
+        
         //critical section
         printf("Data read by the reader%d is %d\n",f,data);
         sleep(1);
         //critical section
+        sem_post(&writeblock);
 
+        sem_wait(&mutex);
+        readers_in_cs--;
+        if(readers_in_cs == 0) {
+            sem_post(&writeblock);
+        }
+        sem_post(&mutex);
+
+        
         usleep(200);
     }
 }
@@ -28,12 +46,14 @@ void *writer(void *arg)
     f = *((int*) arg);
     while(1){
 
+        sem_wait(&writeblock);
         //critical section
         printf("Writer%d starts to write..\n",f);
         data++;
         sleep(1);
         printf("Writer%d finishes to write %d\n",f,data);
         //critical section
+        sem_post(&writeblock);
         
         sleep(1);
     }
