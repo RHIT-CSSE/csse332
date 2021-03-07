56f404d8f58c9aa27a2488e3602260e910a5a000
Mohammad <noureddi@rose-hulman.edu>
2021-03-07 12:37:02 -0500

    fixed version of markdown schedule with easy navigation

--- Admin/gen_schedule.py
+++ Admin/gen_schedule.py
@@ -72,6 +72,7 @@ def determine_starting_week(schedule):
     else:
         return 1
 
+
 def count_weeks(schedule, starting_week):
     """
     Count the number of weeks in the quarter and determine their spans.
@@ -105,6 +106,7 @@ def count_weeks(schedule, starting_week):
 
     return week_counter
 
+
 def write_schedule_header(f):
     """
     Write the header for the schedule in the markdown file.
@@ -115,20 +117,22 @@ def write_schedule_header(f):
     :return: None.
     """
     header = """---
-layout = page
-title = Class Schedule
+layout: page
+title: Class Schedule
 ---
 
-# Class Schedule
+**Jump to week**: [0](#w0) [1](#w1) [2](#w2) [3](#w3) [4](#w4) [5](#w5) [6](#w6) [7](#w7) [8](#w8) [9](#w9) [10](#w10)
+
 <table>
 <colgroup>
 <col width="10%" />
-<col width="10%" />
-<col width="20%" />
-<col width="40%" />
-<col width="20%" />
+<col width="22.5%" />
+<col width="22.5%" />
+<col width="22.5%" />
+<col width="22.5%" />
 </colgroup>"""
     f.write(header)
+
 
 def write_table_header(f):
     """
@@ -139,17 +143,148 @@ def write_table_header(f):
 
     :return: None.
     """
+    # TODO: Make this one call to write. This is bad but helpful when debugging
     f.write("<thead>\n")
     f.write("<tr class=\"header\">\n")
     f.write("<th> Week </th>\n")
     f.write("<th> Session </th>\n")
     f.write("<th> Reading </th>\n")
     f.write("<th> Topics </th>\n")
-    f.write("<th< Material </th>\n")
+    f.write("<th> Material </th>\n")
     f.write("</tr>\n")
     f.write("</thead>\n")
 
+def get_session_date(session):
+    """
+    Get the session date in datetime format
 
+    :param: session: The dictionary representing the session
+    :type: session: {Str:Str}
+
+    :return: A datetime.datetime representation of the session's date.
+    :rtype: datetime.datetime
+    """
+    try:
+        session_date = parse_time_str(session['date'])
+        return session_date
+    except KeyError:
+        logging.error("Could not parse a session's date, process failing...")
+        raise RuntimeError("Failed to parse session date!")
+
+def is_class_session(session):
+    """
+    Check is a session has a class_session type.
+
+    :param: session: The session to check.
+    :type: session: {Str:Str}
+
+    :return: True if the session is of class_session type, False otherwise.
+    :rtype: bool
+    """
+    try:
+        session_type = session['type']
+        return session_type == 'class_session'
+    except KeyError:
+        return False
+
+def start_column(f, options=None):
+    if options:
+        f.write("<td align=\"center\" markdown=\"span\" {}>\n".format(options))
+    else:
+        f.write("<td align=\"center\" markdown=\"span\">\n")
+
+
+def end_column(f):
+    f.write("</td>\n")
+
+
+
+def write_table_body(f, schedule, num_of_sessions_per_week, classes):
+    """
+    Write the table body based on the type of the session and so on.
+
+    :param: f: the file we are writing to.
+    :param: schedule: the list of entries as a list of dictionaries.
+    :param: num_of_sessions_per_week: the list detailing the number of sessions each week contains.
+    :param: classes: the list of class sessions with materials and links.
+
+    :type: f: File
+    :type: schedule: [{Str:Str}]
+    :type: num_of_sessions_per_week: [int]
+
+    :return: None
+    """
+    previous_week = -1
+    for session in schedule:
+        date = get_session_date(session) 
+        week = session['week']
+        f.write("<tr>\n")
+
+        # check if we need a new week column
+        if week != previous_week:
+            f.write("""<td rowspan="{1}" markdown="span" align="center"><a name=\"w{0}\">{0}</a></td>\n""".format(
+                week, num_of_sessions_per_week[week]))
+
+
+        # now dump the actual stuff
+        previous_week = week
+        if is_class_session(session):
+            session_num = session['class_num']
+            # write the date
+            start_column(f)
+            f.write("{} <br/> {}".format(session_num, date.strftime("%a, %b %d %Y")))
+            end_column(f)
+
+            try:
+                logging.debug("Checking session {}".format(session_num))
+                class_content = classes[session_num]
+                # check for reading material
+                if class_content['reading']:
+                    start_column(f)
+                    f.write("{::nomarkdown}<ul>\n")
+                    for item in class_content['reading']:
+                        f.write("<li> {} </li>\n".format(item))
+                    f.write("</ul>{:/}\n")
+                    end_column(f)
+                else:
+                    start_column(f)
+                    end_column(f)
+                
+                # check for topics
+                if class_content['topics']:
+                    start_column(f)
+                    f.write("{::nomarkdown}<ul>\n")
+                    for topic in class_content['topics']:
+                        f.write("<li> {} </li>\n".format(topic))
+                    f.write("</ul>{:/}\n")
+                    end_column(f)
+                else:
+                    start_column(f)
+                    end_column(f)
+
+                # check for materials
+                start_column(f)
+                
+                if class_content['materials']:
+                    if type(class_content['materials']) is str:
+                        f.write(class_content['materials'] + "<br/>")
+                    else:
+                        f.write("{::nomarkdown}<ul>\n")
+                        for material in class_content['materials']:
+                            f.write("<li>{}</li>".format(material))
+                        f.write("</ul>{:/}\n")
+
+                # finally check for others
+                if class_content['other']:
+                    f.write('<br/>'.join(class_content['other']))
+
+                end_column(f)
+
+            except KeyError:
+                f.write("<td align=\"center\" markdown=\"span\" colspan=\"3\">N/A</td>\n")
+        else:
+            f.write("<td align=\"center\" markdown=\"span\" colspan=\"4\">N/A</td>\n")
+        f.write("</tr>\n")
 
 # %%
 if __name__ == '__main__':
@@ -159,6 +294,7 @@ if __name__ == '__main__':
     date_convert_path = os.path.join(source_root, '_data', 'date_convert.yml')
     assignments_path = os.path.join(source_root, '_data', 'assignments.csv')
     schedule_path = os.path.join(source_root, '_data', 'schedule.yml')
+    output_path = os.path.join(source_root, 'schedule.md')
 
     # schedule is a list of dictionnaries.
     schedule = []
@@ -171,6 +307,9 @@ if __name__ == '__main__':
         list_of_dicts = [x for x in reader if x['name'][0] != '#']
         schedule.extend(list_of_dicts)
 
+    with open(schedule_path, 'r') as f:
+        classes = yaml.full_load(f)
+
     logging.info("There are {} calendar entries between assignments and class sessions"
                  .format(len(schedule)))
 
@@ -179,6 +318,25 @@ if __name__ == '__main__':
     starting_week = determine_starting_week(schedule)
     logging.info("Starting week is {}".format(starting_week))
     num_of_sessions_per_week = count_weeks(schedule, starting_week)
+
+    logging.info("Starting to write schedule to {}...".format(output_path))
+    with open(output_path, 'w') as f:
+        write_schedule_header(f)
+        write_table_header(f)
+
+        # >> begin table body
+        f.write("<tbody>\n")
+
+        # main work starts here
+        write_table_body(f, schedule, num_of_sessions_per_week, classes)
+
+        # << close off table body
+        f.write("</tbody>\n")
+
+        # << close off the table
+        f.write("</table>\n")
+
+    logging.info("Done....")
 
 
 # %%
