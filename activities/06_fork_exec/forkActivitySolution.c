b435360e59d8f4b57dd9cee2626963d8076a958e
Mohammad <noureddi@rose-hulman.edu>
2021-03-17 18:35:58 -0400

    added fork activity

---
+++ activities/06_fork_exec/forkActivitySolution.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main() {
+  // look at forkExample for help!
+  int i = 0;
+  for(i = 0;i < 10; i++){
+          int fresult = fork();
+
+          if(fresult == 0){
+
+              // uncomment this code if you'd like to see the solution
+              // to Part 1
+        //       printf("I'm child %d\n", i);
+        //       sleep(2);
+        //       exit(i);
+              
+              char output[50];
+              snprintf(output, 50, "I'm child %d",i);
+              execlp("./buffalosay.bin", "./buffalosay.bin", output, NULL);
+              perror("error execing!");
+              exit(99);                      
+          }
+  }
+  int status;
+  for(i = 0;i < 10; i++){
+          wait(&status);
+          printf("One child returns with exit code %d\n",WEXITSTATUS(status));
+  }
+  printf("exiting\n");
+
+  return 0;
+}
