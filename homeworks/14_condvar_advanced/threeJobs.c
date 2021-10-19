0c98c2269786cafb36f2f787b572e5a21e8d15c6
Mohammad N <noureddi@rose-hulman.edu>
2021-10-19 16:56:50 -0400

    updated skeleton for homework 14

--- homeworks/14_condvar_advanced/threeJobs.c
+++ homeworks/14_condvar_advanced/threeJobs.c
@@ -13,66 +13,66 @@
 #define NUM_DECO 3
 
 /**
-   Imagine there is a shared memory space called house.
+  Imagine there is a shared memory space called house.
 
-   There are 3 different kinds of operations on house: carpenters,
-   painters, and decorators.  For any particular kind of operation,
-   there can be an unlimited number of threads doing the same
-   operation at once (e.g. unlimited carpenter threads etc.).
-   However, only one kind of operation can be done at a time (so even
-   a single carpenter should block all painters and vice versa).
+  There are 3 different kinds of operations on house: carpenters,
+  painters, and decorators.  For any particular kind of operation,
+  there can be an unlimited number of threads doing the same
+  operation at once (e.g. unlimited carpenter threads etc.).
+  However, only one kind of operation can be done at a time (so even
+  a single carpenter should block all painters and vice versa).
 
-   Use semaphores to enforce this constraint.  You don't have to worry
-   about starvation (e.g. that constantly arriving decorators might
-   prevent carpenters from ever running) - though maybe it would be
-   fun to consider how you would solve in that case.
+  Use semaphores to enforce this constraint.  You don't have to worry
+  about starvation (e.g. that constantly arriving decorators might
+  prevent carpenters from ever running) - though maybe it would be
+  fun to consider how you would solve in that case.
 
-   This is similar to the readers/writers problem BTW.
-**/
+  This is similar to the readers/writers problem BTW.
+ **/
 
 void* carpenter(void * ignored) {
 
-    printf("starting carpentry\n");
-    sleep(1);
-    printf("finished carpentry\n");
-    return NULL;
+	printf("starting carpentry\n");
+	sleep(1);
+	printf("finished carpentry\n");
+	return NULL;
 }
 
 void* painter(void * ignored) {
 
-    printf("starting painting\n");
-    sleep(1);
-    printf("finished painting\n");
-    return NULL;
+	printf("starting painting\n");
+	sleep(1);
+	printf("finished painting\n");
+	return NULL;
 }
 
 void* decorator(void * ignored) {
 
-    printf("starting decorating\n");
-    sleep(1);
-    printf("finished decorating\n");
-    return NULL;
+	printf("starting decorating\n");
+	sleep(1);
+	printf("finished decorating\n");
+	return NULL;
 }
 
 
 int main(int argc, char **argv) {
-  pthread_t jobs[NUM_CARP + NUM_PAIN + NUM_DECO];
-    for (int i = 0; i < NUM_CARP + NUM_PAIN + NUM_DECO; i++) {
-        void* (*func) (void*) = NULL;
-        if(i < NUM_CARP)
-            func = carpenter;
-        if(i >= NUM_CARP && i < NUM_CARP + NUM_PAIN)
-            func = painter;
-        if(i >= NUM_CARP + NUM_PAIN) {
-            func = decorator;
-        }
-        pthread_create(&jobs[i], NULL, func, NULL);
-    }
+	pthread_t jobs[NUM_CARP + NUM_PAIN + NUM_DECO];
+	for (int i = 0; i < NUM_CARP + NUM_PAIN + NUM_DECO; i++) {
+		void* (*func) (void*) = NULL;
+		if(i < NUM_CARP)
+			func = carpenter;
+		if(i >= NUM_CARP && i < NUM_CARP + NUM_PAIN)
+			func = painter;
+		if(i >= NUM_CARP + NUM_PAIN) {
+			func = decorator;
+		}
+		pthread_create(&jobs[i], NULL, func, NULL);
+	}
 
-    for (int i = 0; i < NUM_CARP + NUM_PAIN + NUM_DECO; i++) {
-        pthread_join(jobs[i], NULL);
-    }
+	for (int i = 0; i < NUM_CARP + NUM_PAIN + NUM_DECO; i++) {
+		pthread_join(jobs[i], NULL);
+	}
 
-    printf("Everything finished.\n");
+	printf("Everything finished.\n");
 
 }
