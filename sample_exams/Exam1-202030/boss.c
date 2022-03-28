d476135064f8e4e546e3e500d3eaf4e04e9bb328
Michael Hewner <hewner@gmail.com>
2022-03-28 11:07:25 -0400

    removing solution code from boss.c

--- sample_exams/Exam1-202030/boss.c
+++ sample_exams/Exam1-202030/boss.c
@@ -8,71 +8,10 @@
 
 #define NUM_WORKERS 3
 
-static int *glob_var;
 
-
-void make_child() {
-    int fork_result = fork();
-    if(fork_result == -1) {
-        perror("fork failure");
-        exit(1);
-    }
-    if(fork_result > 0) {
-        return;
-    }
-    printf("worker process (pid %d) started\n", getpid());
-
-    signal(SIGCHLD, SIG_IGN);
-    while(1) {
-        fork_result = fork();
-        if(fork_result == -1) {
-            perror("fork failure");
-            exit(1);
-        }
-                
-        if(fork_result > 0) {
-            int status;
-            wait(&status);
-            if(WEXITSTATUS(status) == 99) {
-                printf("task.bin crashed with exit 99. worker (pid %d) aborting\n", getpid());
-                exit(0);
-            }
-            *glob_var = *glob_var + 1;
-            continue;
-        }
-
-        execl("./task.bin", "./task.bin", NULL);
-        perror("exec failure");
-        exit(1);
-    }
-
-    printf("bad place\n");
-}
-
-void child_died() {
-    printf("signal handler\n");
-    make_child();
-}
-
-int main(int argc, char** argv) {
+void main(int argc, char** argv) {
 
     printf("boss process (pid %d) started\n", getpid());
-
-    signal(SIGCHLD, child_died);
-    glob_var = mmap(NULL, sizeof *glob_var, PROT_READ | PROT_WRITE, 
-                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
-
-    *glob_var = 0;
-    int old_glob = 0;
-    for(int i = 0; i < NUM_WORKERS; i++) {
-        make_child();
-    }
-
-    while(1) {
-        if(*glob_var != old_glob) {
-            old_glob++;
-            printf("%d tasks finished\n", old_glob);
-        }
-    }
+    
+    
 }
