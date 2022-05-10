c6d528ddbc272a13c0c8e979168707b8ca6287cb
Mohammad Noureddine <noureddi@rose-hulman.edu>
2022-05-10 10:03:50 -0400

    added the makefile and the stack for the memory protection

---
+++ activities/30_mem_protection/stack.c
@@ -0,0 +1,21 @@
+void
+function(int a, int b, int c)
+{
+	char buffer1[6];
+	char buffer2[10];
+
+  buffer1[0] = 'a';
+  buffer2[0] = 'h';
+  buffer2[1] = 'e';
+  buffer2[2] = 'l';
+  buffer2[3] = 'l';
+  buffer2[4] = 'o';
+  buffer2[5] = 0;
+
+}
+
+void
+main(int argc, char **argv)
+{
+	function(1, 2, 3);
+}
