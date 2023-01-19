2582c5f81193f5b6705f8075289eb13d3a13c994
Mohammad <noureddi@rose-hulman.edu>
2023-01-19 07:42:51 -0500

    moved thread abc into session 22 if we have time

--- notes/session22/thread_ABC.c
+++ notes/session22/thread_ABC.c
@@ -9,41 +9,21 @@
 // Use condition variables to make  A, B, C print out in order.
 // HINT: You need more than one condition variables
 
-int adone = 0;
-int bdone = 0;
-
-pthread_cond_t condA = PTHREAD_COND_INITIALIZER;
-pthread_cond_t condB = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread_func_A(void* arg) {
   sleep(3);
-  pthread_mutex_lock(&lock);
   printf("A\n");
-  adone = 1;
-  pthread_cond_signal(&condA);
-  pthread_mutex_unlock(&lock);
   return NULL;
 }
 
 void* thread_func_B(void* arg) {
   sleep(2);
-  pthread_mutex_lock(&lock);
-  while(!adone)
-    pthread_cond_wait(&condA, &lock);
   // the lock is held by B
   printf("B\n");
-  bdone = 1;
-  pthread_cond_signal(&condB);
-  pthread_mutex_unlock(&lock);
   return NULL;
 }
 void* thread_func_C(void* arg) {
-  pthread_mutex_lock(&lock);
-  while(!bdone)
-    pthread_cond_wait(&condB, &lock);
   printf("C\n");
-  pthread_mutex_unlock(&lock);
   return NULL;
 }
 
