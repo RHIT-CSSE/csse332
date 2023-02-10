12e5ef75014e79df6429de5bd73b4d1c2f7bd68a
Mohammad <noureddi@rose-hulman.edu>
2023-02-10 08:48:08 -0500

    added python3 version of setcookie3

---
+++ labs/lab13/setcookie3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+import sys
+import hashlib
+
+if len(sys.argv) < 2:
+    print("Usage: %s <username1> <username2> ..." % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+
+with open('cookie', 'w') as f:
+    names = '|'.join(sorted(sys.argv[1:]))
+    hashstr = hashlib.sha1(names.encode('utf-8')).hexdigest()
+    hashint = int(hashstr, 16) % 0x8FFF
+    f.write(str(hashint))
