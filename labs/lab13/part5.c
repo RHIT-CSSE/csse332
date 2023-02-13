ded40515b83124e26bf8b786cded182746e4974f
Mohammad <noureddi@rose-hulman.edu>
2023-02-13 16:22:08 -0500

    updated fix for part5

--- labs/lab13/part5.c
+++ labs/lab13/part5.c
@@ -28,8 +28,8 @@ _main(int argc, char **argv)
 
   tmp = argv[1];
   if(argc == 3) {
-    len = strlen(argv[1]) + strlen(argv[2]);
-    tmp = malloc(len + 2);
+    len = strlen(argv[1]) + strlen(argv[2]) + 2;
+    tmp = malloc(len);
     memcpy(tmp, argv[1], strlen(argv[1]));
     tmp[strlen(argv[1])] = 0x20;
     memcpy(tmp + strlen(argv[1]) + 1, argv[2], strlen(argv[2]));
