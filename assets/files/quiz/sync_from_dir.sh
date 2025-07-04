9562e6ef48b5fb35396b6a46385c2b40b5c7ce70
Mohammad Noureddine <sajed@Mohammads-MBP.attlocal.net>
2025-07-03 22:43:06 -0400

    adjusted schedule and added quiz pages for week 05

--- assets/files/quiz/sync_from_dir.sh
+++ assets/files/quiz/sync_from_dir.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 
-pdf_dir="/Users/mohammad/git/github/courses/csse332/csse332-private/Quiz/pdfs"
+# pdf_dir="/Users/sajed/Git/Github/courses/csse332/csse332-private/Quiz/pdfs"
+pdf_dir=$(find ${HOME} -name 'csse332-private' 2> /dev/null)
+pdf_dir="${pdf_dir}/Quiz/pdfs"
+
+echo -e ${pdf_dir}
 
 rsync -avP "$pdf_dir/" .
-
-
-
-
-
-
-
-
-
-
